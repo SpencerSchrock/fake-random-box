551bccf565a06cd62132d98116d2de9f336f1a42
Victor Sui <victorsui11@gmail.com>
2016-10-23 03:23:09 -0500

    add point module

---
+++ js/app/point.js
@@ -0,0 +1,24 @@
+define(function() {
+	console.log("Loaded point!");
+
+	return function(param0, param1, fromCanvas) {
+		if (fromCanvas) {
+			// create point from coordinates of canvas
+			// on-click event
+			// param0 = event
+			// param1 = canvas
+
+			var rect = param1.getBoundingClientRect();
+			this.x = (param0.clientX - rect.left) / param1.width;
+			this.y = (param0.clientY - rect.top) / param1.height;
+
+		} else {
+			// create point with given coordinates
+			// param0 = x
+			// param1 = y
+
+			this.x = param0;
+			this.y = param1;
+		}
+	}
+});
